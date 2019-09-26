4d35b4b40110459f597e5e2b40d4f16b04aa289e
이정환 <46296754+winterlood@users.noreply.github.com>
2019-09-26 14:41:34 +0900

    Create ControlBlock

---
+++ ControlBlock/ControlBlock/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TypressPacket;
+
+namespace ControlBlock
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        DataPacket dp = new DataPacket();
+
+
+        public void getDataPacketFromServer()
+        {
+            // server 에서 datapacket 수신하여 dp 변수에 넣으면 됩니다.
+            // dp = server에서 받아온 Datapacekt;
+
+            dp.Id = "winterlood";
+            dp.Money = Int32.Parse("12000");
+            dp.TotalUsage = Int32.Parse("12000");
+            dp.OneWeekUsage = Int32.Parse("12000");
+            dp.TwoWeekUsage = Int32.Parse("12000");
+            dp.ThreeWeekUsage = Int32.Parse("12000");
+        }
+
+        public DataPacket getNowDataPacket()
+        {
+            // 각 window instance에서 해당 함수를 호출하여 DataPacket을 불러와 사용함
+            return dp;
+        }
+    }
+}
---
+++ ControlBlock/ControlBlock/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ControlBlock.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlBlock
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseLeftButtonDown += MoveWindow;
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+            this.DataContext = new ControlBlockViewModel();
+        }
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
+        }
+        void MoveWindow(object sender, MouseEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
