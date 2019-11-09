b7c37e1c3f9f6ea33e74eee5eced958ea4b33bd2
jongwuner <jklh0202@naver.com>
2019-11-09 22:29:24 +0900

    Update DvPrinter.cs

--- Server-Service/MyService/DvPrinter.cs
+++ Server-Service/MyService/DvPrinter.cs
@@ -10,6 +10,7 @@ namespace MyService
 {
     class DvPrinter
     {
+        int PageCnt;
         string num;
         NktSpyMgr _spyMgr;
         NktProcess _process;
@@ -31,11 +32,19 @@ namespace MyService
 
             //hookPrinter = _spyMgr.CreateHook("spoolsv.exe!PrvStartDocPrinterW", (int)(eNktHookFlags.flgRestrictAutoHookToSameExecutable | eNktHookFlags.flgOnlyPreCall));
 
+            //hookPrinter = _spyMgr.CreateHook("spoolsv.exe!StartDocPrinterW", (int)eNktHookFlags.flgOnlyPreCall);
+            //hookPrinter = _spyMgr.CreateHook("winspool.drv!StartDocPrinterW", (int)eNktHookFlags.flgOnlyPreCall);
+
             hookPrinter = _spyMgr.CreateHook("spoolsv.exe!PrvStartDocPrinterW", (int)eNktHookFlags.flgOnlyPreCall);
             hookPrinter.OnFunctionCalled += OnFunctionCalledPrinter;
 
+            hookPage = _spyMgr.CreateHook("winspool.drv!StartPagePrinter", (int)eNktHookFlags.flgOnlyPreCall);
+            hookPage.OnFunctionCalled += OnFunctionCalledPrintPage;
+
             hookPrinter.Hook(true);
             hookPrinter.Attach(_process, true);
+            hookPage.Hook(true);
+            hookPage.Attach(_process, true);
 
         }
         public void DvPageCount()
@@ -60,10 +69,11 @@ namespace MyService
         private void OnFunctionCalledPrinter(INktHook hhook, INktProcess proc, INktHookCallInfo callInfo)
         {
             TypressService.eventLog1.WriteEntry("Printer Request Event!");
+            PageCnt = 0;
             if (TypressService.packet.IsLogin == false)
             {
                 TypressService.eventLog1.WriteEntry("LoginForm 띄움");
-                System.Diagnostics.Debugger.Launch();
+                //System.Diagnostics.Debugger.Launch();
 
                 string applicationName = "C:\\Users\\jklh0\\source\\github\\Typress\\InterruptLogin\\InterruptLoginView\\InterruptLoginView\\bin\\x64\\Debug\\InterruptLoginView.exe";
                 ApplicationLoader.PROCESS_INFORMATION procInfo;
@@ -73,6 +83,13 @@ namespace MyService
                 //P.WaitForExit(); // IsLogin 변수 바꼈는지?
                 return;
             }
+        }
+
+        private void OnFunctionCalledPrintPage(INktHook hhook, INktProcess proc, INktHookCallInfo callInfo)
+        {
+            PageCnt++;
+            TypressService.eventLog1.WriteEntry("Printer Count Increase Event Page");
+            TypressService.eventLog1.WriteEntry(PageCnt.ToString());
         }
     }
 }
